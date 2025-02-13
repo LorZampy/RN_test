9a58050e7e01c80f11a7551f8f708979e85d5bc7
LorZampy <s298487@studenti.polito.it>
2025-02-13 15:08:01 +0100

    Added boosting, new anims, boosting speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,16 @@ public class PlayerMovement : MonoBehaviour
     [Header("movement")]
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
+    [SerializeField] private float boostSpeed;
+    [SerializeField] private float standardRotationSpeed;
+    [SerializeField] private float boostRotationSpeed;
     private float moveSpeed;
+    private float rotationSpeed;
     [SerializeField] private Transform orientation;
+    [SerializeField] private float boostLoadTime;
+    private float startBoostTime;
+    private bool isRunning;
+    private bool isBoosting;
 
     [SerializeField] private float groundDrag;
 
@@ -30,13 +38,12 @@ public class PlayerMovement : MonoBehaviour
     {
         walking,
         running,
+        boosting,
         airborne
     }    
 
     private float horizontalInput;
     private float verticalInput;
-
-    private bool isRunning;
 
     Vector3 moveDirection;
 
@@ -55,21 +62,32 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
+        // if player is moving walk
         if (horizontalInput != 0 || verticalInput != 0)
         {
             if (!animator.GetBool("isWalking"))
                 { animator.SetBool("isWalking", true); }
         }
+
+        // if player stops moving stop walk, and also run and boost
         else
         {
             if (animator.GetBool("isWalking"))
                 { animator.SetBool("isWalking", false); }
             isRunning = false;
+            isBoosting = false;
             animator.SetBool("isRunning", isRunning);
+            animator.SetBool("isBoosting", isBoosting);
         }
 
+        // if player isn't running start running, else stop. Also stop boosting if it stops running
         if (Input.GetButtonDown("leftStickButton") && animator.GetBool("isWalking") && isGrounded)
         {
+            if (isRunning)
+            { 
+                isBoosting = false;
+                animator.SetBool("isBoosting", isBoosting);
+            }
             isRunning = !isRunning;
             animator.SetBool("isRunning", isRunning);
         }
@@ -83,12 +101,48 @@ public class PlayerMovement : MonoBehaviour
             // call resetJump after a delay equal to jumpCD
             Invoke(nameof(ResetJump), jumpCD);
         }
+
+        // if runnning and grounded get time, else if already boosting stop boosting
+        if (Input.GetButtonDown("westButton") && isRunning && isGrounded)
+        {
+            if (!isBoosting)
+                { startBoostTime = Time.time; }
+            else
+            {
+                isBoosting = false;
+                animator.SetBool("isBoosting", isBoosting);
+            }
+        }
+        // if player held boost button less than required time cancel boost
+        if (Input.GetButtonUp("westButton") && startBoostTime != 0)
+        {
+            if (Time.time - startBoostTime < boostLoadTime)
+            {
+                startBoostTime = 0f;
+            }
+        }
+
+        // if player held boost button long enough enter boost state
+        if (startBoostTime != 0f && Time.time - startBoostTime >= boostLoadTime)
+        {
+            isBoosting = true;
+            animator.SetBool("isBoosting", isBoosting);
+            startBoostTime = 0f;
+        }
+
     }
 
     private void StateHandler()
     {
+        // Boosting state
+        if (isRunning && isBoosting)
+        {
+            moveState = MovementState.boosting;
+            moveSpeed = boostSpeed;
+        }
+
         // Running state
-        if(isGrounded && Input.GetButtonDown("leftStickButton"))
+        else if((isGrounded && Input.GetButtonDown("leftStickButton")) || (isRunning && !isBoosting))
         {
             moveState = MovementState.running;
             moveSpeed = runSpeed;
@@ -99,6 +153,8 @@ public class PlayerMovement : MonoBehaviour
         {
             moveState = MovementState.walking;
             moveSpeed = walkSpeed;
+            isBoosting = false;
+            startBoostTime = 0f;
         }
 
         // Airborne state
